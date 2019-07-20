fbd5ecf2ec6d5424fc3b0f3b36d95cc868983c70
Sorin TKD <sorinmiron@gmail.com>
2019-07-20 11:37:09 +0300

    fixed logic for checking Exit code

--- src/main/java/com/sda/TicketSystem/controller/MainController.java
+++ src/main/java/com/sda/TicketSystem/controller/MainController.java
@@ -85,7 +85,6 @@ public class MainController {
         String exitMessage = getExitMessage(exit_code);
         model.addAttribute("exit_code", exit_code);
         model.addAttribute("exit_message", exitMessage);
-
         return "home";
     }
 
